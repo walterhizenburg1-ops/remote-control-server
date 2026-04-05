5023717d4579c6c35545a897d76c5f066962f8ec
walterhizenburg1-ops <walterhizenburg1@gmail.com>
2026-04-05 06:32:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   res.end('Remote Control Server Running!!');
 });
 
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ server, maxPayload: 10 * 1024 * 1024 });
 const rooms = {};
 
 wss.on('connection', (ws) => {
@@ -14,19 +14,20 @@ wss.on('connection', (ws) => {
   let currentRole = null;
 
   ws.on('message', (message) => {
+    // binary = screenshot frame!!
+    if (message instanceof Buffer) {
+      if (currentRoom && rooms[currentRoom]?.controller) {
+        rooms[currentRoom].controller.send(message);
+      }
+      return;
+    }
+
     const data = JSON.parse(message);
 
     if (data.type === 'join') {
       currentRoom = data.room;
       currentRole = data.role;
-      if (!rooms[currentRoom]) {
-        rooms[currentRoom] = {
-          host: null,
-          controller: null,
-          hostCandidates: [],
-          controllerCandidates: []
-        };
-      }
+      if (!rooms[currentRoom]) rooms[currentRoom] = { host: null, controller: null };
       rooms[currentRoom][currentRole] = ws;
       console.log(`${currentRole} joined room ${currentRoom}`);
 
@@ -34,36 +35,12 @@ wss.on('connection', (ws) => {
       if (rooms[currentRoom][other]) {
         rooms[currentRoom][other].send(JSON.stringify({ type: 'peer-joined', role: currentRole }));
         ws.send(JSON.stringify({ type: 'peer-joined', role: other }));
-
-        // flush buffered candidates!!
-        const myBuffer = currentRole === 'host' ? 'controllerCandidates' : 'hostCandidates';
-        rooms[currentRoom][myBuffer].forEach(c => ws.send(JSON.stringify(c)));
-        rooms[currentRoom][myBuffer] = [];
-      }
-    }
-
-    else if (data.type === 'offer' || data.type === 'answer') {
-      const other = currentRole === 'host' ? 'controller' : 'host';
-      if (rooms[currentRoom]?.[other]) {
-        rooms[currentRoom][other].send(JSON.stringify(data));
-      }
-    }
-
-    else if (data.type === 'ice') {
-      const other = currentRole === 'host' ? 'controller' : 'host';
-      if (rooms[currentRoom]?.[other]) {
-        rooms[currentRoom][other].send(JSON.stringify(data));
-      } else {
-        // buffer it until other side connects!!
-        const buffer = currentRole === 'host' ? 'hostCandidates' : 'controllerCandidates';
-        rooms[currentRoom][buffer].push(data);
       }
     }
 
     else if (data.type === 'touch' || data.type === 'keyboard') {
-      const other = currentRole === 'host' ? 'controller' : 'host';
-      if (rooms[currentRoom]?.[other]) {
-        rooms[currentRoom][other].send(JSON.stringify(data));
+      if (rooms[currentRoom]?.host) {
+        rooms[currentRoom].host.send(JSON.stringify(data));
       }
     }
   });
@@ -72,7 +49,7 @@ wss.on('connection', (ws) => {
     if (currentRoom && rooms[currentRoom]) {
       delete rooms[currentRoom][currentRole];
       const other = currentRole === 'host' ? 'controller' : 'host';
-      if (rooms[currentRoom][other]) {
+      if (rooms[currentRoom]?.[other]) {
         rooms[currentRoom][other].send(JSON.stringify({ type: 'peer-left' }));
       }
     }
@@ -80,4 +57,4 @@ wss.on('connection', (ws) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running!!`));
